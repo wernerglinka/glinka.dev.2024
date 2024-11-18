1a46037767d895887e670ae0325041cac8632d5c
Werner Glinka <werner@glinka.co>
2024-11-17 19:45:28 -0600

    added metalsmith-optimize-html new plugin

--- metalsmith-prod.js
+++ metalsmith-prod.js
@@ -1,12 +1,29 @@
 import drafts from "@metalsmith/drafts";
 //import htmlMinifier from "metalsmith-html-minifier";
+import htmlOptimize from "metalsmith-optimize-html";
 import { msBuild } from './metalsmith.js';
 
 const ms = msBuild();
+
+// set DEBUG environment variable
+//ms.env( 'DEBUG', 'metalsmith-htmlOptimize' );
+
 // filter out draft for production
 ms.use( drafts() );
-// minify HTML in production
-//ms.use( htmlMinifier() );
+// optimize HTML in production
+// home page with htmlOptimizer is 28% smaller (11/17/2024)
+ms.use( htmlOptimize( {
+  "removeComments": true,
+  "removeTagSpaces": true,
+  "removeDefaultAttributes": true,
+  "normalizeBooleanAttributes": true,
+  "cleanUrlAttributes": true,
+  "cleanDataAttributes": true,
+  "removeEmptyAttributes": true,
+  "removeProtocols": true,
+  "simplifyDoctype": true
+} ) );
+
 
 ms.build( err => {
   if ( err ) {
