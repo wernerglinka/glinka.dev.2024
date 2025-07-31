027bfad6a477ec08a3e3c2835ae30fe83b47c6f3
Werner Glinka <werner@glinka.co>
2025-07-31 13:36:46 -0500

    chore: update dependencies

--- src/js/modules/tabs.js
+++ src/js/modules/tabs.js
@@ -3,70 +3,70 @@
  * @params {*} none
  * @return {function} initializes a tabs display
  */
-const tabs = (function() {
+const tabs = ( function() {
   const init = () => {
-    const allTabsContainers = document.querySelectorAll(".js-tabs");
-    
-    allTabsContainers.forEach(tabsContainer => {
-      const allTabs = tabsContainer.querySelectorAll(".tab-label");
-      const allTabContents = tabsContainer.querySelectorAll(".tab-content");
+    const allTabsContainers = document.querySelectorAll( ".js-tabs" );
+
+    allTabsContainers.forEach( tabsContainer => {
+      const allTabs = tabsContainer.querySelectorAll( ".tab-label" );
+      const allTabContents = tabsContainer.querySelectorAll( ".tab-content" );
 
       // loop through all tab contents and find the tallest one
       let tallestTabContent = 0;
-      allTabContents.forEach(tabContent => {
-        if (tabContent.offsetHeight > tallestTabContent) {
+      allTabContents.forEach( tabContent => {
+        if ( tabContent.offsetHeight > tallestTabContent ) {
           tallestTabContent = tabContent.offsetHeight;
         }
-      });
+      } );
       // set tab contents to the height of the tallest tab content
-      const tabsContent = document.querySelector(".tabs-content");
-      if (tabsContent) {
-        document.querySelector(".tabs-content").style.height = `${tallestTabContent}px`;
+      const tabsContent = document.querySelector( ".tabs-content" );
+      if ( tabsContent ) {
+        document.querySelector( ".tabs-content" ).style.height = `${ tallestTabContent }px`;
       };
 
       // observe viewport width changes and update tab content height
-      const resizeObserver = new ResizeObserver((entries) => {
-        entries.forEach(entry => {
+      const resizeObserver = new ResizeObserver( ( entries ) => {
+        entries.forEach( entry => {
           // loop through all tab contents and find the tallest one
           let tallestTabContent = 0;
-          allTabContents.forEach(tabContent => {
-            if (tabContent.offsetHeight > tallestTabContent) {
+          allTabContents.forEach( tabContent => {
+            if ( tabContent.offsetHeight > tallestTabContent ) {
               tallestTabContent = tabContent.offsetHeight;
             }
-          });
+          } );
           // set tab contents to the height of the tallest tab content
-          const tabsContent = document.querySelector(".tabs-content");
-          if (tabsContent) {
-            document.querySelector(".tabs-content").style.height = `${tallestTabContent}px`;
+          const tabsContent = document.querySelector( ".tabs-content" );
+          if ( tabsContent ) {
+            document.querySelector( ".tabs-content" ).style.height = `${ tallestTabContent }px`;
           };
-        });
-      });
+        } );
+      } );
 
-      resizeObserver.observe(document.body);
+      resizeObserver.observe( document.body );
 
-      allTabs.forEach(tab => {
-        tab.addEventListener("click", (e) => {
+      allTabs.forEach( tab => {
+        tab.addEventListener( "click", ( e ) => {
           e.stopPropagation();
           e.preventDefault();
 
-          allTabs.forEach(tab => {
-            tab.classList.remove("active");
-            tab.children[0].removeAttribute("aria-selected");
-          });
-          tab.classList.add("active");
-          tab.children[0].setAttribute("aria-selected", "true");
+          allTabs.forEach( tab => {
+            tab.classList.remove( "active" );
+            tab.children[ 0 ].removeAttribute( "aria-selected" );
+          } );
+          tab.classList.add( "active" );
+          tab.children[ 0 ].setAttribute( "aria-selected", "true" );
           // convert allTabs nodelist to array and get index of clicked tab
-          const clickedTabIndex = Array.prototype.slice.call(allTabs).indexOf(tab);
+          const clickedTabIndex = Array.prototype.slice.call( allTabs ).indexOf( tab );
           // remove active class from all tab contents
-          allTabContents.forEach(tabContent => tabContent.classList.remove("active"));
+          allTabContents.forEach( tabContent => tabContent.classList.remove( "active" ) );
           // add active class to tab content with same index as clicked tab
-          allTabContents[clickedTabIndex].classList.add("active");
-        });
-      });
-    });
+          allTabContents[ clickedTabIndex ].classList.add( "active" );
+        } );
+      } );
+    } );
   };
 
   return { init };
-})();
+} )();
 
 export default tabs;
